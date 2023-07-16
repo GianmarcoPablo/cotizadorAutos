0f816438afc5ec1dc5adbd51ecce6508fab27e02
Gian Marco <Gianmarcopablo588@gmail.com>
2023-07-15 23:19:48 -0500

    fin

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,9 +1,33 @@
+import { useCallback,useMemo,useRef } from "react"
 import useCotizador from "../hooks/useCotizador"
+import {MARCAS,PLANES} from "../constants"
 const Resultado = () => {
     const {resultado,datos} = useCotizador()
+    const {marca,year,plan} = datos
+    const yearRef = useRef(year)
+    const [nombreMarca] = useCallback(MARCAS.filter(m=>m.id === Number(marca)),[resultado])
+    const [nombrePlan] = useCallback(PLANES.filter(p=>p.id === Number(plan)),[resultado] )
+
     if(resultado === 0) return null
     return (
-        <div>Resultado</div>
+        <div className="bg-gray-200 text-center mt-5 p-5 shadow">
+            <h2 className="text-gray-600 font-black text-3xl">
+                Resumen
+            </h2>
+            <p className="my-2">
+                <span className="font-bold text-gray-800">Marca: </span> {nombreMarca.nombre}
+            </p>
+            <p className="my-2">
+                <span className="font-bold text-gray-800">Año: </span> { yearRef.current}
+            </p>
+            <p className="my-2">
+                <span className="font-bold text-gray-800">Plan: </span> {nombrePlan.nombre}
+            </p>
+
+            <p className="my-2 text-2xl">
+                <span className="font-bold text-gray-800">Total: </span> {resultado}
+            </p>
+        </div>
     )
 }
 
